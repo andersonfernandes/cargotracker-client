a98ad7c95589a0aa6abfb60f838882ad5d88ce22
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-11-17 20:31:46 -0300

    Update Track and Dashboard pages
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/pages/cargo/Dashboard.js
+++ src/pages/cargo/Dashboard.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
+import { format } from 'date-fns';
+
 import AdminMenu from "../../components/AdminMenu";
 
 export default function CargoDashboard() {
+  const [cargos, setCargos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://cargotracker-api.up.railway.app/cargos')
+      .then(response => response.json())
+      .then(data => setCargos(data));
+  }, []);
+
   return (
     <>
       <AdminMenu />
 
-      <h1>Dashboard</h1>
+      <h1>Cargas registradas</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Tracking ID</th>
+            <th>Source</th>
+            <th>Destination</th>
+            <th>Deadline</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cargos.map(cargo => (
+            <tr key={cargo._id}>
+              <td>{cargo.trackingId}</td>
+              <td>{cargo.source.name}</td>
+              <td>{cargo.destination.name}</td>
+              <td>{format(new Date(cargo.deadlineAt), 'MM/dd/yyyy')}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }

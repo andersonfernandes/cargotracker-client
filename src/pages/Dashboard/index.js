e888227d1449c32dcd6d49a1d5cc95c7268881ca
Matheus Cassiano <matheuscassiano9@gmail.com>
2022-12-06 12:03:42 -0300

    feat: adicionando loading ao dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,15 +4,22 @@ import Container from "../../components/Container";
 import { AdminMenu } from "../../components/Menus/AdminMenu";
 import { Card } from "./components/Card";
 
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 import "./style.css";
 
 export default function CargoDashboard() {
   const [cargos, setCargos] = useState([]);
+  const [isLoaging, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://cargotracker-api.up.railway.app/cargos")
       .then((response) => response.json())
-      .then(setCargos);
+      .then((res) => {
+        setCargos(res);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -23,23 +30,59 @@ export default function CargoDashboard() {
         <div className="cargo-container">
           <div className="cargo-wrapper">
             <h2 className="cargo-section-title">Routed Cargo</h2>
-            <section className="cargo-grid">
-              {cargos.map((cargo) => (
-                <Card key={cargo._id} cargo={cargo} />
-              ))}
-            </section>
+
+            {!isLoaging ? (
+              <section className="cargo-grid">
+                {cargos.map((cargo) => (
+                  <Card key={cargo._id} cargo={cargo} />
+                ))}
+              </section>
+            ) : (
+              <Skeleton
+                containerClassName="cargo-grid"
+                baseColor="#181818"
+                highlightColor="#2C2D2D"
+                height={312}
+                inline={true}
+                count={3}
+              />
+            )}
             <h2 className="cargo-section-title">Not Routed Cargo</h2>
-            <section className="cargo-grid">
-              {cargos.map((cargo) => (
-                <Card key={cargo._id} cargo={cargo} />
-              ))}
-            </section>
+
+            {!isLoaging ? (
+              <section className="cargo-grid">
+                {cargos.map((cargo) => (
+                  <Card key={cargo._id} cargo={cargo} />
+                ))}
+              </section>
+            ) : (
+              <Skeleton
+                containerClassName="cargo-grid"
+                baseColor="#181818"
+                highlightColor="#2C2D2D"
+                height={312}
+                inline={true}
+                count={3}
+              />
+            )}
             <h2 className="cargo-section-title">Claimed Cargo</h2>
-            <section className="cargo-grid">
-              {cargos.map((cargo) => (
-                <Card key={cargo._id} cargo={cargo} />
-              ))}
-            </section>
+
+            {!isLoaging ? (
+              <section className="cargo-grid">
+                {cargos.map((cargo) => (
+                  <Card key={cargo._id} cargo={cargo} />
+                ))}
+              </section>
+            ) : (
+              <Skeleton
+                containerClassName="cargo-grid"
+                baseColor="#181818"
+                highlightColor="#2C2D2D"
+                height={312}
+                inline={true}
+                count={3}
+              />
+            )}
           </div>
         </div>
       </main>
